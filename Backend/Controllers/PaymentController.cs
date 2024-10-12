a82d802163ad5d36be7ea9b0d6014a49e49d0feb
Amir380-A <62997533+Amir380-A@users.noreply.github.com>
2024-10-12 13:06:29 +0300

    Add Stripe

---
+++ Backend/Controllers/PaymentController.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+[Route("api/[controller]")]
+[ApiController]
+public class PaymentsController : ControllerBase
+{
+    private readonly PaymentService _paymentService;
+
+    public PaymentsController(PaymentService paymentService)
+    {
+        _paymentService = paymentService;
+    }
+
+    [HttpPost("checkout-session")]
+    public async Task<IActionResult> CreateCheckoutSession(decimal amount, string currency)
+    {
+        var session = await _paymentService.CreateCheckoutSession(amount, currency,
+            successUrl: "https://localhost:5129/api/success",
+            cancelUrl: "https://localhost:5129/api/cancel");
+
+        return Ok(new { id = session.Id });
+    }
+}
+
+
